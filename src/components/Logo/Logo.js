abb335d0ef84f08725f1cd62cc48778746f334a9
DandyDo <alqasimtoon@gmail.com>
2021-07-09 19:51:18 -0400

    added components, tachyons, and react-tilty

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import Tilty from 'react-tilty';
+import brain from './brain.png';
+import './Logo.css';
 
 const Logo = () => {
     return (
       <div className= 'ma4 mt0'>
-        <Tilt className="Tilt" options={{ max : 25 }} style={{ height: 250, width: 250 }} >
-            <div className="Tilt-inner"> 👽 </div>
-        </Tilt>
+        <Tilty className="w-100 Tilty flex items-center justify-center br2 shadow-2" perspective={1300}  style={{ height: 250, width: 250 }}>
+          <div>
+            <img className='w-100' alt='logo' src={ brain }/>
+          </div>
+        </Tilty>
       </div>  
     );
 }
-
 
 export default Logo;
