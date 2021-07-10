d84f06f666137f154f575798cda2b42101a4d9c8
DandyDo <alqasimtoon@gmail.com>
2021-07-10 17:27:55 -0400

    Added some styles to css files, added ImageLinkform component

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -6,7 +6,7 @@ import './Logo.css';
 const Logo = () => {
     return (
       <div className= 'ma4 mt0'>
-        <Tilty className="Tilty h-100 flex items-center justify-center br2 shadow-2" perspective={1300}  style={{ height: 180, width: 180 }}>
+        <Tilty className="Tilty h-100 flex items-center justify-center br2 shadow-2" perspective={1300} style={{ height: 180, width: 180 }}>
           <div>
             <img alt='logo' src={ brain }/>
           </div>
