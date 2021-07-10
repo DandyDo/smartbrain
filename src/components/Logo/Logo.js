b219f5e7852390e78401210373c696d9afd40c86
DandyDo <alqasimtoon@gmail.com>
2021-07-09 23:39:41 -0400

    added favicon, changed title, deleted some extra fiels, and minimal refactoring for Logo component

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -6,9 +6,9 @@ import './Logo.css';
 const Logo = () => {
     return (
       <div className= 'ma4 mt0'>
-        <Tilty className="w-100 Tilty flex items-center justify-center br2 shadow-2" perspective={1300}  style={{ height: 250, width: 250 }}>
+        <Tilty className="Tilty h-100 flex items-center justify-center br2 shadow-2" perspective={1300}  style={{ height: 180, width: 180 }}>
           <div>
-            <img className='w-100' alt='logo' src={ brain }/>
+            <img alt='logo' src={ brain }/>
           </div>
         </Tilty>
       </div>  
