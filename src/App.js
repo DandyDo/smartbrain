8254e983c7e81a363bf1df1a2b85e2a874b49359
DandyDo <alqasimtoon@gmail.com>
2021-08-30 23:35:57 -0400

    fixed issue where data does not reset when loggin out and logging in as a different user

--- src/App.js
+++ src/App.js
@@ -93,6 +93,21 @@ const app = new Clarifai.App({
   apiKey: 'Your API Key',
 });
 
+const initialState = {
+  input: '',
+  imageUrl:'',
+  boxes: [],
+  route: 'signin',
+  isSignedIn: false,
+  user: {
+    id: '',
+    name: '',
+    email: '',
+    entries: 0,
+    joined: ''
+  }
+}
+
 class App extends Component {
   constructor() {
     super();
@@ -169,7 +184,8 @@ loadUser = (data) => {
         .then(response => response.json())
         .then(count => {
           this.setState(Object.assign(this.state.user, { entries: count }))
-        });
+        })
+        .catch(console.log);
       }
       
       this.displayFaceBox(this.calculateFaceLocation(response))
@@ -179,7 +195,7 @@ loadUser = (data) => {
 
   onRouteChange = (route) => {
     if (route === 'signout') {
-      this.setState({ isSignedIn: false });
+      this.setState(initialState);
     }
     else if (route === 'home'){
       this.setState({ isSignedIn: true });
